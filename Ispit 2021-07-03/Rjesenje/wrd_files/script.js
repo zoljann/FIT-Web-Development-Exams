19eb9f3100d7f2c407ba5e99632dd4b9f127493c
Nedim Zolj <74002374+zoljann@users.noreply.github.com>
2021-07-22 03:47:59 +0200

    UpdateFetch

--- Ispit 2021-07-03/Rjesenje/wrd_files/script.js
+++ Ispit 2021-07-03/Rjesenje/wrd_files/script.js
@@ -1,33 +1,33 @@
 $.validator.addMethod(
     "regex",
-    function(value, element, regexp) {
+    function (value, element, regexp) {
         var check = false;
         return this.optional(element) || regexp.test(value);
     },
     "Provjeri unos!<br>"
 );
 $("#forma").validate({
-    rules:{
-        naslov:{
-            required:true,
+    rules: {
+        naslov: {
+            required: true,
             regex: /^[A-Z][a-zA-Z]+\s[a-zA-Z]+$/
         },
-        telefon:{
-            required:true,
-            regex:/^\+[0-9]{3}-[0-9]{2}-[0-9]{3}-[0-9]{4}$/
+        telefon: {
+            required: true,
+            regex: /^\+[0-9]{3}-[0-9]{2}-[0-9]{3}-[0-9]{4}$/
         }
     },
-    messages:{
-        naslov:{
-            required:"Obavezno polje!",
-            regex:"Minimalno dvije riječi. Prva riječ počinje velikim slovom.",
-        }, 
-        telefon:{
-            required:"Obavezno polje!",
-            regex:"Format +111-11-111-1111",
+    messages: {
+        naslov: {
+            required: "Obavezno polje!",
+            regex: "Minimalno dvije riječi. Prva riječ počinje velikim slovom.",
         },
-        poruka:{
-            required:"Obavezno polje!",
+        telefon: {
+            required: "Obavezno polje!",
+            regex: "Format +111-11-111-1111",
+        },
+        poruka: {
+            required: "Obavezno polje!",
         }
     }
 })
@@ -35,8 +35,7 @@ getPodatke = (postavi) => {
     fetch("https://restapiexample.wrd.app.fit.ba/Ispit20210702/Get4Studenta")
         .then((r) => {
             if (r.status != 200) {
-                console.log("Doslo je do greske!");
-                return;
+                alert("Doslo je do greske ");
             }
 
             r.json().then((x) => {
@@ -48,7 +47,7 @@ getPodatke = (postavi) => {
             alert(error);
         });
 }
-kreirajRadnika = (obj) =>{
+kreirajRadnika = (obj) => {
     return `<img src="${obj.slikaPutanja}"/>
     <div id="tekstRadnika">
         <h3>${obj.imePrezime}</h3>
@@ -56,15 +55,49 @@ kreirajRadnika = (obj) =>{
         <button>Piši poruku</button>
     </div>`;
 }
-postaviRadnika = (obj)=>{
-    var dohvatiRadnika= document.querySelectorAll("[id^=radnik]")
-    for(let i=0; i<obj.length; i++){
-        dohvatiRadnika[i].innerHTML=kreirajRadnika(obj[i]);
+postaviRadnika = (obj) => {
+    var dohvatiRadnika = document.querySelectorAll("[id^=radnik]")
+    for (let i = 0; i < obj.length; i++) {
+        dohvatiRadnika[i].innerHTML = kreirajRadnika(obj[i]);
     }
 }
 getPodatke(postaviRadnika);
 
 
-$("#IzbornikDugme").on("click", function(){ //padajuci meni za telefon
+posaljiPoruku = (podaci) => {
+    fetch("https://restapiexample.wrd.app.fit.ba/Ispit20210702/Add", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(podaci),
+    })
+        .then((r) => {
+            if (r.status != 200) {
+                alert("Server javlja gresku: " + r.status);
+                return;
+            }
+
+            r.json().then(x => {
+                alert("Doslo je do greske " + error);
+            });
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+};
+
+posalji = () => {
+    let poruka = {
+        imePrezime: document.getElementById("primaoc").value(),
+        naslov: document.getElementById("naslov").value,
+        telefon: document.getElementById("telefon").value,
+        poruka: document.getElementById("poruka").value,
+    };
+    posaljiPoruku(poruka);
+};
+
+
+$("#IzbornikDugme").on("click", function () { //padajuci meni za telefon
     $("#Izbornik").toggle();
 })
